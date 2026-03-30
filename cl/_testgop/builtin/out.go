db05c0338caaaaaa1a57a66e1051305a70706a56
xushiwei <x@goplus.org>
2026-03-31 07:26:15 +0800

    revert #2670

--- cl/_testgop/builtin/out.go
+++ cl/_testgop/builtin/out.go
@@ -3,12 +3,9 @@ package main
 import (
 	"fmt"
 	"github.com/qiniu/x/stringslice"
-	"github.com/qiniu/x/stringutil"
 )
 
 func main() {
 	a := []string{"hello", "world", "123"}
 	fmt.Println(stringslice.Capitalize(a))
-	fmt.Println(stringutil.Contains("param-required required", "required"))
-	fmt.Println(stringutil.Contains("param-required required", "param"))
 }
